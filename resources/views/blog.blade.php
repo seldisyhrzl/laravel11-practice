575f7ec1934677dcd0b5d732a194296393a10d47
seldisyhrzl <seldysz114@gmail.com>
2025-02-12 12:55:15 +0700

    View Data

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-  <x-slot:title>{{ $title }}</x-slot:title>
-  <h3>Welcome to My Blog</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,54 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'HomePage']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Seldi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit ratione quam obcaecati corporis omnis placeat sapiente facilis eaque quo dolorum a laboriosam architecto illo, dolorem eum vero repudiandae voluptates.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rizal',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit ratione quam obcaecati corporis omnis placeat sapiente facilis eaque quo dolorum a laboriosam architecto illo, dolorem eum vero repudiandae voluptates.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Seldi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit ratione quam obcaecati corporis omnis placeat sapiente facilis eaque quo dolorum a laboriosam architecto illo, dolorem eum vero repudiandae voluptates.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rizal',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit ratione quam obcaecati corporis omnis placeat sapiente facilis eaque quo dolorum a laboriosam architecto illo, dolorem eum vero repudiandae voluptates.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
