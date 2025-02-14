e3b7613dc84003c71c24abba812e2d9113265377
seldisyhrzl <seldysz114@gmail.com>
2025-02-14 14:03:02 +0700

    add Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,38 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-  public static function all()
-  {
-    return [
-      [
-        'id' => 1,
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1',
-        'author' => 'Seldi',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit ratione quam obcaecati corporis omnis placeat sapiente facilis eaque quo dolorum a laboriosam architecto illo, dolorem eum vero repudiandae voluptates.'
-      ],
-      [
-        'id' => 2,
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2',
-        'author' => 'Rizal',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit ratione quam obcaecati corporis omnis placeat sapiente facilis eaque quo dolorum a laboriosam architecto illo, dolorem eum vero repudiandae voluptates.'
-      ]
-    ];
-  }
-
-  public static function find($slug): array
-  {
-    $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-    if (!$post) {
-      abort(404);
-    }
-
-    return $post;
-  }
+  protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
     <a href="/posts/{{ $post['slug'] }}" class="hover:underline"><h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2></a>
 
     <div class="text-base text-gray-500">
-      <a href="#">{{ $post['author'] }}</a> | 1 Feb 2025
+      <a href="#">{{ $post['author'] }}</a>  | {{ $post->created_at->diffForHumans() }}
     </div>
     <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
     <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
